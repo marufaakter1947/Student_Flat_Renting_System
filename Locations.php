9f6361a40744a6da459c6b78e975cfe2ab9eeaec
marufaakter1947 <marufaakter1947@gmail.com>
2024-11-07 00:04:54 +0600

    Add files via upload

---
+++ Locations.php
@@ -0,0 +1,8 @@
+<select name="location">
+    <option value="choose" selected> Choose A Location... </option>
+    <option value="Shib Barir Mor"> Shib Barir Mor </option>
+    <option value="New Market"> New Market </option>
+    <option value="Boyra"> Boyra </option>
+    <option value="Sonadanga"> Sonadanga </option>
+    <option value="ShantiDham"> ShantiDham </option>
+</select>
